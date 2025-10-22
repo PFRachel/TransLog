ddda9977f85f31c3927c3dbc6795214a40f61847
Rachel <166075283+PFRachel@users.noreply.github.com>
2025-10-22 16:22:23 -0600

    Delete main.pl

--- main.pl
+++
@@ -1,5 +0,0 @@
-/* =========================================================
-   Archivo: main.pl
-   Proyecto: TransLog
-   Descripcion: Interfaz de consola para interactuar con el sistema.
-   ========================================================= */
